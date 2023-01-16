4255dc3d9cba1a20f1f7f1661c7b47245212e6ce
D Rao <tdayakarrao@gmail.com>
2023-01-17 02:51:25 +0530

    W1PA3: Calculate Perimeter and Area of a Circle

---
+++ NPTEL_Java_March_2023/src/W1PA/CirclePerimeterArea.java
@@ -0,0 +1,29 @@
+package W1PA;
+
+import java.util.Scanner;  
+
+
+public class CirclePerimeterArea {
+	
+	public static void main(String[] args) {
+	
+	
+	Scanner s = new Scanner(System.in); 
+	       double radius= s.nextDouble();
+	       double perimeter;
+	       double area;
+	       
+	   	//Calculate the perimeter
+
+			perimeter = (2 * Math.PI) * radius;        
+
+	  	//Calculate the area
+
+			area = (Math.PI * radius) * radius;
+			
+			System.out.println(perimeter);
+	        System.out.print(area);
+	}
+
+
+}
